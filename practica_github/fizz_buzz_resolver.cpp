d108c13027863ff0c3b0ddfcaba0b45115b44334
Jazmin Gamboa Chacon <jazmin.gamboa.chacon@est.una.ac.cr>
2024-01-04 11:18:21 -0600

    Added the  function initArray()

--- practica_github/fizz_buzz_resolver.cpp
+++ practica_github/fizz_buzz_resolver.cpp
@@ -1,1 +1,10 @@
 #include "fizz_buzz_resolver.h"
+
+void FizzBuzzResolver::initArray()
+{
+	for (int position = 0; position < SIZE; position++)
+	{
+		first100NumberList[position] = position + 1;
+		cout << first100NumberList[position] << " ";
+	}
+}
--- practica_github/fizz_buzz_resolver.h
+++ practica_github/fizz_buzz_resolver.h
@@ -3,9 +3,15 @@
 
 using namespace std;
 
+const int SIZE = 100;
+const int DIVISOR_OR_THREE = 3;
+const int DIVISOR_OR_FIVE = 5;
+
 class FizzBuzzResolver
 {
 private:
+	int first100NumberList[SIZE];
+	void initArray();
 
 public:
 };
